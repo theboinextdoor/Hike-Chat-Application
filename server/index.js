a61d059cab450d6139d2813a9ff03187ed6ee4c5
Damon <cman55245@gmail.com>
2024-06-06 15:17:04 +0530

    made some changes on deployment code

--- server/index.js
+++ server/index.js
@@ -6,18 +6,13 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import authRoute from "./routes/auth.route.js"
 import userRoute from "./routes/userSearch.route.js"
+import connectToMongoDB from "./database/connectToMogoDB.js"
 import { app, server } from "./socket/user.socket.js"
 
-// MONGODB CONNECTION FILE
-import connectToMongoDB from "./database/connectToMogoDB.js"
 
-// initialzing the dotenv configuration to fetch the environmental variable
-dotenv.config();
-
-// initializing all the essentilas for the backend server
-// const app = express();
 const PORT = process.env.PORT || 3000;
-console.log("Port number : ", process.env.PORT)
+const __dirname = path.resolve();
+dotenv.config();
 
 app.use(cors({
      origin: 'http://localhost:3000', // Replace with your frontend domain
@@ -36,29 +31,10 @@ app.use(cookieParser())
 app.use("/api/auth", authRoute)          // API for the authenticatioin user
 app.use("/api/user", userRoute)          // API for the Searching user
 
-
-// Deploying starts here
-
-const __dirname = path.resolve();
-const environment = process.env.NODE_ENV;
-console.log("Environment Production : ",environment)
-if (environment === "production") {
-     // Serve static files from the React app
-    
-     app.use(express.static(path.join(__dirname, "/client/dist")));
-     
-     // Handle React routing, return all requests to React app
-     app.get('*', (req, res) => {
-          res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-     });
-} else {
-     app.get("/", (req, res) => {
-          res.send("API is running Succcessfully......")
-     })
-
-}
-
-// Deploying ends here
+app.use(express.static(path.join(__dirname , "/client/dist")))
+app.get("*" , (req , res) =>{
+     app.use(express.static(path.join(__dirname , "client" , "dist" , "index.html")))
+})
 
 
 // connecting to the mongodb and starting the server
